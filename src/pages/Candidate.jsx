7d7b792529212dd55830a4bfe956cacca96cb46b
Akshay200228 <sankpla2812@gmail.com>
2023-07-08 17:28:03 +0530

    Update framer motion

--- src/pages/Candidate.jsx
+++ src/pages/Candidate.jsx
@@ -39,12 +39,31 @@ const Candidate = () => {
       transition={{ duration: 0.5 }}
     >
       <div className="w-full px-4">
-        <h2 className="mb-4 text-2xl font-bold text-white">Search Candidates</h2>
+        <motion.h2 
+          className="mb-4 text-2xl font-bold text-white"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          Search Candidates
+        </motion.h2>
         <div className="flex flex-col items-center mb-4 lg:flex-row">
-          <label htmlFor="locationSelect" className="w-auto mb-2 lg:mb-0 lg:mr-2 text-dimWhite">
+          {/* Location Select */}
+          <motion.label
+            htmlFor="locationSelect"
+            className="w-auto mb-2 lg:mb-0 lg:mr-2 text-dimWhite"
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1 }}
+          >
             Location:
-          </label>
-          <div className="w-full mb-4 xl:mb-0 lg:w-1/2 lg:ml-2">
+          </motion.label>
+          <motion.div
+            className="w-full mb-4 xl:mb-0 lg:w-1/2 lg:ml-2"
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8 }}
+          >
             <div className="relative">
               <Select
                 id="locationSelect"
@@ -56,9 +75,16 @@ const Candidate = () => {
                 placeholder="Select location..."
               />
             </div>
-          </div>
+          </motion.div>
+
+          {/* Job Role Input */}
           <div className="w-full mb-2 xl:mb-0 lg:w-1/2 lg:ml-2">
-            <div className="relative">
+            <motion.div
+              className="relative"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
               <input
                 id="jobRoleInput"
                 type="text"
@@ -73,33 +99,51 @@ const Candidate = () => {
               >
                 Job Role
               </label>
-            </div>
+            </motion.div>
           </div>
         </div>
-        
-        <div className="flex flex-col lg:flex-row">
+
+        {/* Search and Clear Filters Buttons */}
+        <motion.div 
+          className="flex flex-col lg:flex-row"
+        >
           <button
             onClick={handleSearch}
-            className="w-full px-4 py-2 mt-2 font-bold text-white bg-blue-500 rounded lg:w-auto lg:mt-0 lg:ml-2 hover:bg-blue-700 btn whitespace-nowrap"
-          >
+            className="w-full px-4 py-2 mt-2 font-bold text-white bg-[#03DAC6] rounded lg:w-auto lg:mt-0 lg:ml-2 hover:bg-[#018786] btn whitespace-nowrap"
+            >
             Search
           </button>
           <button
             onClick={handleClearFilters}
-            className="w-full px-4 py-2 mt-2 font-bold text-white bg-red-500 rounded lg:w-auto lg:mt-0 lg:ml-2 hover:bg-red-700 btn whitespace-nowrap"
-          >
+            className="w-full px-4 py-2 mt-2 font-bold text-white bg-[#B00020] rounded lg:w-auto lg:mt-0 lg:ml-2 hover:bg-red-900 btn whitespace-nowrap"
+            >
             Clear Filters
           </button>
-        </div>
+        </motion.div>
 
         <hr className="my-6 border-gray-500" />
 
-        <h2 className="mt-4 text-2xl font-bold text-white">Candidate List</h2>
+        <motion.h2
+          className="mt-4 text-2xl font-bold text-white"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          Candidate List
+        </motion.h2>
+
+        {/* Candidate Cards */}
         <div className="flex flex-wrap mt-4 overflow-y-auto text-white" style={{ maxHeight: 'calc(100vh - 300px)' }}>
           {filteredCandidates.map((candidate) => (
-            <div key={candidate.id} className="w-full p-2 sm:w-1/2 lg:w-1/3">
-              <div className="bg-[#29347a] rounded shadow">
-                <div className="flex items-center p-4">
+            <motion.div
+              key={candidate.id}
+              className="w-full p-2 sm:w-1/2 lg:w-1/3"
+              initial={{ opacity: 0, scale: 0.2 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }}
+            >
+              <div className="bg-[#29347a] rounded shadow p-4">
+                <div className="flex items-center mb-4">
                   <img
                     src={candidate.image}
                     alt={`Candidate ${candidate.id}`}
@@ -108,16 +152,16 @@ const Candidate = () => {
                   <div className="flex flex-col">
                     <h3 className="text-lg font-semibold">{candidate.name}</h3>
                     <p className="text-dimWhite">{candidate.jobRole}</p>
-                    <div className="mt-4">
-                      <p className="text-sm text-dimWhite">Contact Information:</p>
-                      <p className="truncate">{candidate.email}</p>
-                      <p>{candidate.phone}</p>
-                    </div>
                   </div>
                 </div>
-                <p className="p-4 mt-2">{candidate.description}</p>
+                <div className="mb-2 text-sm text-dimWhite">
+                  <p className="mb-1">Contact Information:</p>
+                  <p className="truncate">{candidate.email}</p>
+                  <p>{candidate.phone}</p>
+                </div>
+                <p>{candidate.description}</p>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
